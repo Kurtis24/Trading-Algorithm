adf2ceabe8332d3d15661b3b9819125ba7e7f33a
KurtisL7 <kurtislin24@gmail.com>
2025-01-08 13:30:18 -0500

    Figured out I should only do, one graph and not mutiple to start small

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
-# import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    df1 = pd.read_csv('information_MSFT.csv')
-    df2 = pd.read_csv('information_AAPL.csv')
-    df3 = pd.read_csv('information_GOOGL.csv')
+    df1 = pd.read_csv('information_GOOGL.csv')
     
     # Convert "Date" columns to datetime
     df1['Date'] = pd.to_datetime(df1['Date'])
-    df2['Date'] = pd.to_datetime(df2['Date'])
-    df3['Date'] = pd.to_datetime(df3['Date'])
-
-    plt.figure(figsize=(10,6))
+    
+    plt.figure(figsize=(10,5))
     
     # Plot scatter of each
-    plt.scatter(df1['Date'], df1['Volume'], c="red",   linewidth=2, label="MSFT Volume")
-    plt.scatter(df2['Date'], df2['Volume'], c="green", linewidth=2, label="AAPL Volume")
-    plt.scatter(df3['Date'], df3['Volume'], c="black", linewidth=2, label="GOOGL Volume")
+    plt.scatter(df1['Date'], df1['Volume'], c="red",   linewidth=2, label="GOOGL Volume")
 
-    df_all = pd.concat([
-        df1[['Date', 'Volume']],
-        df2[['Date', 'Volume']],
-        df3[['Date', 'Volume']]
-    ], ignore_index=True)
+    df1['Date_ordinal'] = df1['Date'].map(pd.Timestamp.toordinal)
+    
+    x = df1['Date_ordinal'].values.reshape(-1, 1)
+    y = df1['Volume'].values.reshape(-1, 1)   
 
-    df_all.sort_values(by='Date', inplace=True)
+    X = np.hstack([x, np.ones_like(x)])          
 
-    df_all['Date_ordinal'] = df_all['Date'].map(pd.Timestamp.toordinal)
+    theta = np.linalg.inv(X.T @ X) @ (X.T @ y)   
 
-    x = df_all['Date_ordinal'].values.reshape(-1, 1)
-    y = df_all['Volume'].values.reshape(-1, 1)
+    y_line = X @ theta                           
 
-    X = np.hstack([x, np.ones_like(x)])
+    plt.plot(df1['Date'], y_line, 'r', label='GOOGL Best Fit')
 
-    theta = np.linalg.inv(X.T @ X) @ (X.T @ y)
-
-    y_pred = X @ theta
-
-    plt.plot(df_all['Date'], y_pred, color='purple', linewidth=2,
-             label='Combined Best Fit')
-
-    # 10) Label, legend, show
-    plt.title('Volume Over Time - Combined Regression')
+    plt.title('Volume Over Time')
     plt.xlabel('Date')
     plt.ylabel('Volume')
     plt.xticks(rotation=45)
